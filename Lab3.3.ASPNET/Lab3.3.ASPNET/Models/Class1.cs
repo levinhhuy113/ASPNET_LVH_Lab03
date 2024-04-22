deea750869df38e39a9f2afab052f7cb796bda5d
unknown <levinhhuy113@gmail.com>
2024-04-22 19:52:17 +0700

    add file

---
+++ Lab3.3.ASPNET/Lab3.3.ASPNET/Models/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab3._3.ASPNET.Models
+{
+    public class Country
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
+}
